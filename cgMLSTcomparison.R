e2af51fc1d1f14e47313af3a90fcdc81da99b0aa
Nicolas Radomski <nicolas.radomski.phd@gmail.com>
2021-10-05 12:16:28 +0200

    Update cgMLSTcomparison.R

--- cgMLSTcomparison.R
+++ cgMLSTcomparison.R
@@ -4,7 +4,7 @@
 rm(list=ls())
 
 # set working directory
-setwd("/home/Downloads/cgMLSTcomparison")
+setwd("/PathTo/RstudioWorkingDirectory/cgMLSTcomparison")
 
 # install regular packages
 install.packages("ggplot2")
